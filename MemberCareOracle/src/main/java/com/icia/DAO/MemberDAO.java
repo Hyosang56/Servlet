166a7cefb4dd891ac1389f7f7466a117ac452eb7
Hyosang56 <jhsaa01@naver.com>
2024-09-09 21:33:57 +0900

    Login

--- MemberCareOracle/src/main/java/com/icia/DAO/MemberDAO.java
+++ MemberCareOracle/src/main/java/com/icia/DAO/MemberDAO.java
@@ -1,10 +1,14 @@
 package com.icia.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.icia.vo.MemberVo;
 
 public class MemberDAO {
 	private MemberDAO() {      
@@ -30,4 +34,65 @@ public class MemberDAO {
 	      
 	      return  conn ;
 	   }
+	   public int confirmID(String userid) {
+		      int result = -1;
+		      String sql = "select userid from member where userid=?";
+		      Connection conn = null;
+		      PreparedStatement pstmt = null;
+		      ResultSet rs = null;
+		      try {
+		         conn = getConnection();
+		         pstmt = conn.prepareStatement(sql);
+		         pstmt.setString(1, userid);
+		         rs = pstmt.executeQuery();
+		         if (rs.next()) {
+		            result = 1;
+		         } else {
+		            result = -1;
+		         }
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		      } finally {
+		         try {
+		            if (rs != null)
+		               rs.close();
+		            if (pstmt != null)
+		               pstmt.close();
+		            if (conn != null)
+		               conn.close();
+		         } catch (Exception e) {
+		            e.printStackTrace();
+		         }
+		      }
+		      return result;
+		   }//end of confirmID()
+	   public int insertMember(MemberVo mVo) {
+		      int result = -1;
+		      String sql = "insert into member values(?, ?, ?, ?, ?, ?)";
+		      Connection conn = null;
+		      PreparedStatement pstmt = null;
+		      try {
+		         conn = getConnection();
+		         pstmt = conn.prepareStatement(sql);
+		         pstmt.setString(1, mVo.getName());
+		         pstmt.setString(2, mVo.getUserid());
+		         pstmt.setString(3, mVo.getPwd());
+		         pstmt.setString(4, mVo.getEmail());
+		         pstmt.setString(5, mVo.getPhone());
+		         pstmt.setInt(6, mVo.getAdmin());
+		         result = pstmt.executeUpdate();
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		      } finally {
+		         try {
+		            if (pstmt != null)
+		               pstmt.close();
+		            if (conn != null)
+		               conn.close();
+		         } catch (Exception e) {
+		            e.printStackTrace();
+		         }
+		      }
+		      return result;
+		   }//end of insertMember()
 }
---
+++ MemberCareOracle/src/main/java/com/icia/controller/IdCheckServlet.java
@@ -0,0 +1,61 @@
+package com.icia.controller;
+
+import java.io.IOException;
+
+import com.icia.DAO.MemberDAO;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IdCheckServlet
+ */
+public class IdCheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IdCheckServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userid = 
+	            request.getParameter("userid");
+	      MemberDAO mDao = MemberDAO.getInstance();
+	      int result = mDao.confirmID(userid);
+	      request.setAttribute("userid", userid);
+	      request.setAttribute("result", result);
+	      RequestDispatcher dispatcher = request
+	      .getRequestDispatcher("member/idcheck.jsp");
+	      dispatcher.forward(request, response);
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
